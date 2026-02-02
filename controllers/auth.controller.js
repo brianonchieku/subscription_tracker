fb223b75529fe0f711c80bf8cdfe7212131dcafa
machuki <brianonchikeku0@gmail.com>
2026-02-02 23:58:53 +0300

    auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -25,9 +25,9 @@ export const signUp = async (req, res, next) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt)
 
-        const newUser = await User.create([{ name, email, password: hashedPassword}], { session }, {expiresIn: JWT_EXPIRES_IN});
+        const newUser = await User.create([{ name, email, password: hashedPassword}], { session });
 
-        const token = jwt.sign({ userId: newUser[0]._id}, JWT_SECRET)
+        const token = jwt.sign({ userId: newUser[0]._id}, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
         await session.commitTransaction();
         session.endSession();
@@ -50,6 +50,41 @@ export const signUp = async (req, res, next) => {
 
 };
 
-export const signIn = async (req, res, next) => {};
+export const signIn = async (req, res, next) => {
+    try {
+
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ email });
+
+        if(!user){
+            const error = new Error("user does not exist with this email");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid){
+            const error = new Error("Invalid password");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        res.status(200).json({
+            success: true,
+            message: "User signed in successfully",
+            data: {
+                token,
+                user
+            }
+        })
+
+    } catch (error){
+        next(error);
+    }
+};
 
 export const signOut = async (req, res, next) => {};
